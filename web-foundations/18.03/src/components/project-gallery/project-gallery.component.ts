050d8f1a2920ffe5694f475b45a790c33d5afd16
Max Stankevich <max.stankevich.me@gmail.com>
2018-03-23 15:43:58 +0200

    feat: project-gallery navigation bullets + missing theme file

--- web-foundations/18.03/src/components/project-gallery/project-gallery.component.ts
+++ web-foundations/18.03/src/components/project-gallery/project-gallery.component.ts
@@ -1,8 +1,9 @@
 import Swiper from "swiper";
+import {SwiperOptions} from "swiper";
 
 // initialize project gallery swipers
 (function () {
-    const swipers: Swiper[] = new Swiper(".project-gallery .swiper-container", {
+    const options: SwiperOptions = {
         init: false,
         direction: "horizontal",
         runCallbacksOnInit: true,
@@ -10,13 +11,18 @@ import Swiper from "swiper";
             prevEl: ".swiper-button-prev",
             nextEl: ".swiper-button-next",
         },
+        pagination: {
+            clickable: true,
+        },
         // will be enabled if contains more than one slide
         allowSlidePrev: false,
         allowSlideNext: false,
         allowTouchMove: false,
-    }) as any;
+    };
+    const swipers: Swiper[] = new Swiper(".project-gallery .swiper-container", options) as any;
 
-    for (const swiper of swipers) {
+    for (let i = 0; i < swipers.length; i++) {
+        const swiper = swipers[i];
         swiper.once('init', function (this: Swiper) {
             // activate swiping and navigation
             if (this.slides.length > 1) {
@@ -26,8 +32,17 @@ import Swiper from "swiper";
                 this.allowTouchMove = true;
 
                 // show navigation
-                this.navigation.nextEl.classList.remove("hidden");
-                this.navigation.prevEl.classList.remove("hidden");
+                this.navigation.nextEl.classList.remove(this.params.navigation.hiddenClass);
+                this.navigation.prevEl.classList.remove(this.params.navigation.hiddenClass);
+
+                // setup pagination
+                this.params.pagination.el = querySelectorInParents<HTMLElement>(this.el, ".swiper-pagination");
+                this.params.pagination.el.classList.remove(this.params.pagination.hiddenClass);
+                this.pagination.init();
+
+                // render pagination
+                this.pagination.render();
+                this.pagination.bullets[this.activeIndex].classList.add(this.params.pagination.bulletActiveClass);
             }
         });
 
@@ -42,5 +57,13 @@ import Swiper from "swiper";
         });
 
         swiper.init();
+    }
+
+    function querySelectorInParents<E extends Element = Element>(element: Element, selector: string): E | null {
+        // while has parent
+        for (; element; element = element.parentElement) {
+            const query = element.querySelector<E>(selector);
+            if (query) return query;
+        }
     }
 })();
