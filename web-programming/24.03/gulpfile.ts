aee28c09edc3da7d140c1dcece58ad043e22548b
Max Stankevich <max.stankevich.me@gmail.com>
2018-04-28 02:16:39 +0300

    feat: memory game pipeline

---
+++ web-programming/24.03/gulpfile.ts
@@ -0,0 +1,55 @@
+import * as bs from "@browser-sync";
+import {args} from "@cli";
+import {spawn} from "child_process";
+import * as del from "del";
+import * as gulp from "gulp";
+import {parallel, series, task} from "gulp";
+
+function clean() {
+    return del(["./dist"]);
+}
+
+function sync() {
+    const glob = ["./src/**/*.{css,html}"];
+
+    if (args.watch)
+        gulp.watch(glob, sync);
+
+    return gulp.src(glob)
+        .pipe(gulp.dest("./dist"));
+}
+
+function compile() {
+    const params = [...args.flags];
+    const exe = require.resolve(".bin/webpack");
+    return spawn(exe, params, {shell: true, stdio: "inherit"});
+}
+
+function serve() {
+    return bs.serve({server: "./dist"});
+}
+
+function serveFlags(done: Function) {
+    args.flags.push("-w", "--watch");
+    done();
+}
+
+task(clean);
+task(sync);
+task(compile);
+
+task("default", series(
+    clean,
+    parallel(
+        sync,
+        compile,
+    )),
+);
+
+task("serve", series(
+    serveFlags,
+    parallel(
+        "default",
+        serve,
+    ),
+));
