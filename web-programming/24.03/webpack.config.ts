11bb9087ff6101217762b6204692eedb53374ea2
Max Stankevich <max.stankevich.me@gmail.com>
2018-04-30 04:03:44 +0300

    chore: do not inline images in dev mode

--- web-programming/24.03/webpack.config.ts
+++ web-programming/24.03/webpack.config.ts
@@ -1,50 +1,55 @@
+import {WebpackArgv, WebpackEnv} from "@webpack";
 import {CheckerPlugin} from "awesome-typescript-loader";
 import * as ExtractTextPlugin from "extract-text-webpack-plugin";
 import * as path from "path";
 import {Configuration} from "webpack";
 
-const config: Configuration = {
-    mode: "development",
-    entry: {
-        "js/app.js": "./src/js/app",
-        "css/app.css": "./src/css/app",
-    },
-    output: {
-        filename: "[name]",
-        path: path.resolve("./dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-                loader: "url-loader",
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    use: ["css-loader", "postcss-loader"],
-                }),
-            },
-            {
-                test: /\.tsx?$/,
-                use: ["awesome-typescript-loader"],
-            },
-
+export = async function (env: WebpackEnv, argv: WebpackArgv): Promise<Configuration> {
+    const production = argv.mode === "production";
+    const develop = !production;
+    return {
+        mode: "development",
+        entry: {
+            "js/app.js": "./src/js/app",
+            "css/app.css": "./src/css/app",
+        },
+        output: {
+            filename: "[name]",
+            path: path.resolve("./dist"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    loader: "url-loader",
+                    options: {
+                        limit: develop ? 1 : undefined, // do not inline for dev mode
+                        name: "img/[name].[ext]",
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: ["css-loader", "postcss-loader"],
+                    }),
+                },
+                {
+                    test: /\.tsx?$/,
+                    use: ["awesome-typescript-loader"],
+                },
+            ],
+        },
+        resolve: {
+            extensions: [
+                ".ts",
+                ".js",
+                ".css",
+                ".json",
+            ],
+        },
+        plugins: [
+            new CheckerPlugin(),
+            new ExtractTextPlugin("[name]"),
         ],
-
-    },
-    resolve: {
-        extensions: [
-            ".ts",
-            ".js",
-            ".css",
-            ".json",
-        ],
-    },
-    plugins: [
-        new CheckerPlugin(),
-        new ExtractTextPlugin("[name]"),
-    ],
+    };
 };
-
-export = config;
