64156ae224ade5f852fb812a5c1cbabe7030bbfc
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-21 19:40:31 +0300

    fix: webpack.config.ts compilation error

--- exploring-js/13/webpack.config.ts
+++ exploring-js/13/webpack.config.ts
@@ -3,7 +3,7 @@ import {Configuration} from "webpack";
 import sass from "./webpack/webpack-sass.config";
 import ts from "./webpack/webpack-ts.config";
 
-export = async function (env: WebpackEnv = {}, argv: WebpackArgv): Promise<Configuration[]> {
+export = async function (env: WebpackEnv, argv: WebpackArgv): Promise<Configuration[]> {
     return [
         ts(env, argv),
         sass(env, argv),
