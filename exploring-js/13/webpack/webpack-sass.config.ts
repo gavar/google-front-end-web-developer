5e3d78350d620bb9ef2949e3f05b515c7daff875
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-21 18:18:40 +0300

    chore: arcade game webpack config

---
+++ exploring-js/13/webpack/webpack-sass.config.ts
@@ -0,0 +1,48 @@
+import {safeWebpackEnv, WebpackArgv, WebpackEnv} from "@webpack";
+import ExtractTextPlugin from "extract-text-webpack-plugin";
+import path from "path";
+import {Configuration} from "webpack";
+
+export default function (env: WebpackEnv, argv: WebpackArgv): Configuration {
+    env = safeWebpackEnv(env);
+    const urlLoader = env["url-loader"];
+
+    return {
+        entry: {
+            "css/style.css": "./src/css/style",
+        },
+        mode: argv.mode || "development",
+        devtool: argv.devtool || argv.mode !== "production" ? "source-map" : false,
+        output: {
+            filename: "[name]",
+            path: path.resolve("./dist"),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                    loader: "url-loader",
+                    options: {
+                        limit: urlLoader.limit || undefined,
+                        name: "img/[name].[ext]",
+                    },
+                },
+                {
+                    test: /\.css$/,
+                    use: ExtractTextPlugin.extract({
+                        use: ["css-loader", "postcss-loader"],
+                    }),
+                },
+
+            ],
+        },
+        resolve: {
+            extensions: [
+                ".css",
+            ],
+        },
+        plugins: [
+            new ExtractTextPlugin("[name]"),
+        ],
+    };
+};
