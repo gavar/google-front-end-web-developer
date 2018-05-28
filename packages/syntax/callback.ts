c8bcc306b18c679adcb185b72b0a210c249d9700
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-28 18:00:55 +0300

    feat: fix callback syntax

--- packages/syntax/callback.ts
+++ packages/syntax/callback.ts
@@ -1,25 +1,31 @@
-/**
- * Node style callback function.
- * @template T - type of result data.
- * @template E - type of error.
- */
-export interface Callback<T = any, E = Error> {
-
+type EmptyCallback = {
     /**
      * Notify operation successful completion.
      */
     (): void;
+}
+
+type ErrorCallback<E = Error> = {
 
     /**
      * Notify operation failure.
      * @param error - error which provides reason of operation failure.
      */
     (error: E);
+}
 
+type SuccessCallback<T = any, E = Error> = {
     /**
      * Notify operation success.
      * @param error - ignore
      * @param result - operation resulting data.
      */
-    (error: void, result: T): void;
+    (error: void | Error, result: T): void;
 }
+
+/**
+ * Node style callback function.
+ * @template T - type of result data.
+ * @template E - type of error.
+ */
+export type Callback<T = any, E = Error> = EmptyCallback | ErrorCallback<E> | SuccessCallback<T, E>;
