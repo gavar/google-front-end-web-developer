4ac0b76f01d47e333ab65752f6912ab628f9a9fd
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-28 16:02:36 +0300

    feat: more types in syntax

--- packages/syntax/action.ts
+++ packages/syntax/action.ts
@@ -4,3 +4,17 @@
 export interface Action {
     (): void;
 }
+
+/**
+ * Represents an operation that accepts one argument and returns no result.
+ */
+export interface Action1<T> {
+    (arg: T): void;
+}
+
+/**
+ * Represents an operation that accepts two arguments and returns no result.
+ */
+export interface Action2<T1, T2> {
+    (arg1: T1, arg2: T2): void;
+}
--- packages/syntax/index.ts
+++ packages/syntax/index.ts
@@ -1,4 +1,7 @@
+export * from "./with";
 export * from "./action";
+export * from "./newable";
+export * from "./mutable";
 export * from "./callback";
 export * from "./dictionary";
 export * from "./deep-partial";
