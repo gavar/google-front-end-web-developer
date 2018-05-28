521b7b3e7c0127db59995bd1bda51d3871a6ac8e
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-29 00:14:27 +0300

    fix: draw only components on active actors

--- exploring-js/13/src/ts/systems/draw.ts
+++ exploring-js/13/src/ts/systems/draw.ts
@@ -1,6 +1,6 @@
-import {System} from "$engine";
+import {Component, System} from "$engine";
 
-export interface Draw2D {
+export interface Draw2D extends Component {
     draw2D(ctx: CanvasRenderingContext2D): void
 }
 
@@ -26,6 +26,7 @@ export class DrawSystem extends System<Draw2D> {
     protected process(deltaTime: number, components: ReadonlyArray<Draw2D>): void {
         const ctx2D = this.context2D;
         for (const component of components)
-            component.draw2D(ctx2D);
+            if (component.actor.active)
+                component.draw2D(ctx2D);
     }
 }
