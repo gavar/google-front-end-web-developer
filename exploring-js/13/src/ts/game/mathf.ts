eabfc657cd30aa086ddb35681dc363380633fb86
Max Stankevich <max.stankevich.me@gmail.com>
2018-05-29 01:37:23 +0300

    fix: maths randomization

--- exploring-js/13/src/ts/game/mathf.ts
+++ exploring-js/13/src/ts/game/mathf.ts
@@ -1,14 +1,40 @@
 /**
- * Math functions library.
+ * Randomization library.
  */
-export class Mathf {
+export class Random {
+
+    /**
+     * Get random number within given range.
+     * @param min - minimum number inclusive.
+     * @param max - maximum number exclusive.
+     */
+    public static range(min: number, max: number): number {
+        return min + (max - min) * Math.random();
+    }
 
     /**
      * Get random integer number within given range.
      * @param min - minimum number inclusive.
-     * @param max - maximum number inclusive.
+     * @param max - maximum number exclusive.
      */
-    public static randomRangeInt(min: number, max: number): number {
-        return min + Math.floor(((max - min) * Math.random()) + 0.5);
+    public static rangeInt(min: number, max: number): number {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return min + Math.floor((max - min) * Math.random());
     }
+
+    /**
+     * Get random deviation within given tolerance.
+     * @param {number} tolerance - maximum offset from 1.
+     */
+    public static deviation(tolerance: number): number {
+        return 1 + (Math.random() - 0.5) * 2 * tolerance;
+    }
+}
+
+/**
+ * Math functions library.
+ */
+export class Mathf {
+
 }
