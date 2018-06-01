338f331404b808a080c0c4927b4f0b6e0045e593
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-01 18:14:21 +0300

    feat: comparer function interface

--- packages/syntax/index.ts
+++ packages/syntax/index.ts
@@ -2,6 +2,7 @@ export * from "./with";
 export * from "./action";
 export * from "./newable";
 export * from "./mutable";
+export * from "./compare";
 export * from "./callback";
 export * from "./dictionary";
 export * from "./deep-partial";
