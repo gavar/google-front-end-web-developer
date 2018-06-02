89406c7bda232056985080dfa992c9066d1044fe
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-02 17:33:48 +0300

    feat: common vectors

--- exploring-js/13/src/ts/components/vector.ts
+++ exploring-js/13/src/ts/components/vector.ts
@@ -4,6 +4,15 @@
 export interface Vector2 {
     x: number;
     y: number;
+
+}
+
+export namespace Vector2 {
+    export const zero: Readonly<Vector2> = {x: 0, y: 0};
+    export const up: Readonly<Vector2> = {x: 0, y: 1};
+    export const down: Readonly<Vector2> = {x: 0, y: -1};
+    export const left: Readonly<Vector2> = {x: -1, y: 0};
+    export const right: Readonly<Vector2> = {x: 1, y: 0};
 }
 
 /**
