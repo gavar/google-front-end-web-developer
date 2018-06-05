a85da6eb5fc599785d9dc495d66ee1cef87795c8
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-05 03:53:10 +0300

    feat: player console log on collision

--- exploring-js/13/src/ts/game/player.ts
+++ exploring-js/13/src/ts/game/player.ts
@@ -1,7 +1,8 @@
 import {Actor, Component} from "$engine";
 import {View} from "$game";
+import {Collider2D, PhysicsListener2D} from "$physics";
 
-export class Player implements Component {
+export class Player implements Component, PhysicsListener2D {
 
     /** @inheritDoc */
     public readonly actor?: Actor;
@@ -12,5 +13,15 @@ export class Player implements Component {
     start() {
         this.view = this.view || this.actor.require(View);
         this.view.setImage("char-boy.png");
+    }
+
+    /** @inheritDoc */
+    triggerEnter2D(collider: Collider2D): void {
+        console.log("enter", collider.actor.name, collider.actor.id);
+    }
+
+    /** @inheritDoc */
+    triggerExit2D(collider: Collider2D): void {
+        console.log("exit", collider.actor.name, collider.actor.id);
     }
 }
