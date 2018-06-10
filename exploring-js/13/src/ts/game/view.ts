d6a4a07552333af514a2bf477e36702ab8e151d3
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-11 02:45:14 +0300

    feat: view sprite is now accesible

--- exploring-js/13/src/ts/game/view.ts
+++ exploring-js/13/src/ts/game/view.ts
@@ -1,18 +1,20 @@
 import {Resources, Sprite} from "$components";
 import {Actor, Component} from "$engine";
+import {Mutable} from "@syntax";
 
 /**
  * View controls image of a {@link Sprite}.
  */
 export class View implements Component {
 
-    private sprite: Sprite;
-
     /** Resource loader to use. */
     public resources: Resources;
 
     /** @inheritDoc */
     public readonly actor: Actor;
+
+    /** Sprite rendering image of this view. */
+    public readonly sprite: Sprite;
 
     /**
      * Set image to display.
@@ -29,12 +31,12 @@ export class View implements Component {
     }
 
     /** @inheritDoc */
-    awake() {
+    awake(this: Mutable<this>) {
         this.sprite = this.actor.add(Sprite);
     }
 
     /** @inheritDoc */
-    destroy() {
+    destroy(this: Mutable<this>) {
         this.actor.remove(this.sprite);
         this.sprite = null;
     }
