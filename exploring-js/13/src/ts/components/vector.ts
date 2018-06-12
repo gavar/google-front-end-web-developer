8b0298858756c3ad5adffc2841f57333a6e317a8
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-12 03:08:47 +0300

    feat: smoot value

--- exploring-js/13/src/ts/components/vector.ts
+++ exploring-js/13/src/ts/components/vector.ts
@@ -1,3 +1,5 @@
+import {Mathf} from "$game";
+
 /**
  * 2D dimensional vector with X,Y coordinates.
  */
@@ -14,6 +16,19 @@ export namespace Vector2 {
     export const down: Readonly<Vector2> = {x: 0, y: -1};
     export const left: Readonly<Vector2> = {x: -1, y: 0};
     export const right: Readonly<Vector2> = {x: 1, y: 0};
+
+    /**
+     * Gradually change towards a destination value over time.
+     * @param now - current value (modified by this function).
+     * @param to - destination value to achieve.
+     * @param velocity - object to store velocity.
+     * @param smoothTime - approx time of achieving destination value.
+     * @param deltaTime - time since last call of this function.
+     */
+    export function smooth(now: Vector2, to: Readonly<Vector2>, velocity: Vector2, smoothTime: number, deltaTime: number): void {
+        now.x = Mathf.smooth(now.x, to.x, velocity, "x", smoothTime, deltaTime);
+        now.y = Mathf.smooth(now.y, to.y, velocity, "y", smoothTime, deltaTime);
+    }
 }
 
 /**
--- exploring-js/13/src/ts/game/mathf.ts
+++ exploring-js/13/src/ts/game/mathf.ts
@@ -37,4 +37,38 @@ export class Random {
  */
 export class Mathf {
 
+    /**
+     * Gradually change towards a destination value over time.
+     * @param now - current value.
+     * @param to - destination value.
+     * @param velocityObject - container storing current velocity value (modified by this func).
+     * @param velocityKey - name of the property in container to use for storing velocity value.
+     * @param smoothTime - approx time of achieving destination value.
+     * @param deltaTime - time since last call of this function.
+     * @return value to apply to current value.
+     * @see https://github.com/Unity-Technologies/UnityCsReference/blob/11bcfd801fccd2a52b09bb6fd636c1ddcc9f1705/Runtime/Export/Mathf.cs#L303
+     */
+    public static smooth(now: number, to: number, velocityObject: object, velocityKey: string, smoothTime: number, deltaTime: number): number {
+        const o = 2 / smoothTime;
+        const x = o * deltaTime;
+        const e = 1 / (1 + x + 0.48 * x * x + 0.235 * x * x * x);
+
+        const delta = now - to;
+        const destination = to;
+
+        to = now - delta;
+
+        let velocity = velocityObject[velocityKey];
+        let temp = (velocity + o * delta) * deltaTime;
+        velocity = (velocity - o * temp) * e;
+        let next = to + (delta + temp) * e;
+
+        if (destination - now > 0 === next > destination) {
+            next = destination;
+            velocity = (next - destination) / deltaTime;
+        }
+
+        velocityObject[velocityKey] = velocity;
+        return next;
+    }
 }
