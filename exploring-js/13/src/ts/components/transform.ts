3a6c425e82fff77224106ae9e1c36ce859e71780
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-13 21:19:01 +0300

    feat: transform now has scale

--- exploring-js/13/src/ts/components/transform.ts
+++ exploring-js/13/src/ts/components/transform.ts
@@ -6,4 +6,15 @@ import {Vector3} from "./vector";
 export class Transform {
     /** World position of an actor. */
     public position: Vector3 = {x: 0, y: 0, z: 0};
+
+    /** World scale of an actor. */
+    public scale: Vector3 = {x: 1, y: 1, z: 1};
+
+    /** Set value of {@link scale}. */
+    setScale(x: number, y: number, z?: number) {
+        this.scale.x = x;
+        this.scale.y = y;
+        if (arguments.length > 2)
+            this.scale.z = z;
+    }
 }
