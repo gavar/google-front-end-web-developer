99ae131d5d312e64f54b86de059d2990f6b80110
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-17 18:43:30 +0300

    feat: reactive.modify

--- exploring-js/13/src/ts/game/reactive.ts
+++ exploring-js/13/src/ts/game/reactive.ts
@@ -7,16 +7,31 @@ export class Reactive implements Component {
     public readonly actor: Actor;
 
     /**
-     * Modify property of this object and emit {@link GameEvents#PROPERTY_CHANGED} if its value has been changed.
+     * Set property of this object to a given value and emit {@link GameEvents#PROPERTY_CHANGED} if its value has been changed.
      * @param key - name of the property to modify.
      * @param value - new property value.
-     * @return true if property has been changed; otherwise null.
+     * @return true if property has been changed; otherwise false.
      */
     set<K extends keyof this>(key: K, value: this[K]): boolean {
         if (this[key] === value)
             return false;
 
         this[key] = value;
+        this.actor.emit(GameEvents.PROPERTY_CHANGED, this);
+        return true;
+    }
+
+    /**
+     * Modify property of this object by applying delta value and emit {@link GameEvents#PROPERTY_CHANGED} if its value has been changed.
+     * @param key - name of the property to modify.
+     * @param delta - delta value to apply to a current value.
+     * @return true if property has been changed; otherwise false.
+     */
+    modify<K extends keyof this>(key: K, delta: this[K] & number): boolean {
+        if (!delta)
+            return false;
+
+        this[key] += delta as any;
         this.actor.emit(GameEvents.PROPERTY_CHANGED, this);
         return true;
     }
