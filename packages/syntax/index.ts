936d481c1d53a813e92eee3600bf7972dcb21f3e
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-22 23:56:47 +0300

    fix: callback syntax

--- packages/syntax/index.ts
+++ packages/syntax/index.ts
@@ -3,6 +3,6 @@ export * from "./action";
 export * from "./newable";
 export * from "./mutable";
 export * from "./compare";
-export * from "./callback";
+export * from "./nodify";
 export * from "./dictionary";
 export * from "./deep-partial";
---
+++ packages/syntax/nodify.ts
@@ -0,0 +1,30 @@
+/**
+ * Node style completion notificaition.
+ */
+export interface Done<T = any, E = Error> {
+
+    /** Notify operation successful completion. */
+    (): void;
+
+    /**
+     * Notify operation failure.
+     * @param error - error which provides reason of operation failure.
+     */
+    (error: E);
+
+    /**
+     * Notify operation success.
+     * @param error - ignore
+     * @param result - operation resulting data.
+     */
+    (error: never | void | null | 0 | false, result: T): void;
+}
+
+/**
+ * Node style callback consumer.
+ * @template T - type of result data.
+ * @template E - type of error.
+ */
+export type Nodify<T = any, E = Error> = {
+    (error: void | E, value: T): void;
+};
