dffc2d10d86f99e5db820fa27b9541cff723d268
Max Stankevich <max.stankevich.me@gmail.com>
2018-06-25 03:35:36 +0300

    feat: use proxy for sending analytics data to google

--- app/analytics/servlet.ts
+++ app/analytics/servlet.ts
@@ -1,45 +1,13 @@
-import cookieParser from "cookie-parser";
-import express, {NextFunction, Request, Response} from "express";
-import useragent from "express-useragent";
+import express from "express";
+import proxy from "express-http-proxy";
 import {AddressInfo} from "net";
-import got from "got";
-import uuid from "uuid/v4";
 
 const PORT = process.env.PORT || 8000;
 const app = express();
-app.enable("trust proxy");
-app.use(useragent.express());
-app.use(cookieParser());
-app.get("/", function (req: Request, res: Response, next: NextFunction) {
-    track(req, res)
-        .then(x => {
-            res.status(204).send();
-            console.log(x);
-        })
-        .catch(next);
-});
 
-function track(req: Request, res: Response) {
-    const {query, cookies} = req;
-
-    let cid = cookies["cid"];
-    if (!cid) {
-        cid = uuid();
-        res.cookie("cid", cid, {httpOnly: true});
-    }
-
-    // analytics params
-    const data = {
-        v: 1,
-        cid,
-        ...query,
-        ua: req.useragent.source,
-    };
-
-    return got.post("http://www.google-analytics.com/collect", {
-        form: data,
-    });
-}
+// https://medium.freecodecamp.org/save-your-analytics-from-content-blockers-7ee08c6ec7ee
+// proxying requests from /ga to www.google-analytics.com
+app.use("/ga/", proxy("https://www.google-analytics.com"));
 
 const server = app.listen(PORT);
 server.on("error", error);
