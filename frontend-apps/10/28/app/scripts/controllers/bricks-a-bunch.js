5b4fbcb7a741ef3eb2d71032a165af2de808c56a
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-02 02:15:29 +0300

    feat@angular: lesson #28 solution

--- frontend-apps/10/28/app/scripts/controllers/bricks-a-bunch.js
+++ frontend-apps/10/28/app/scripts/controllers/bricks-a-bunch.js
@@ -8,21 +8,10 @@
  * Controller of the serviceQuizApp
  */
 angular.module("serviceQuizApp")
-  .controller("BricksABunchCtrl", function () {
+  .controller("BricksABunchCtrl", ["brickWarehouse", function (warehouse) {
     this.name = "Bricks A Bunch";
-
-    this.redBricks = {
-      "1x1": {
-        quantity: 13,
-        price: 0.01,
-      },
-      "2x2": {
-        quantity: 48,
-        price: 0.03,
-      },
-      "2x6": {
-        quantity: 9,
-        price: 0.05,
-      },
+    this.redBricks = warehouse.bricks.red;
+    this.addToCart = function (size) {
+      warehouse.buy("red", size);
     };
-  });
+  }]);
---
+++ frontend-apps/10/28/app/scripts/services/brickwarehouse.js
@@ -0,0 +1,51 @@
+"use strict";
+
+/**
+ * @ngdoc service
+ * @name serviceQuizApp.brickWarehouse
+ * @description
+ * # brickWarehouse
+ * Service in the serviceQuizApp.
+ */
+angular.module("serviceQuizApp")
+  .service("brickWarehouse", function () {
+    this.bricks = {
+      red: {
+        "1x1": {
+          quantity: 16 + 13,
+          price: 0.01,
+        },
+        "2x2": {
+          quantity: 48,
+          price: 0.03,
+        },
+        "2x6": {
+          quantity: 18 + 9,
+          price: 0.05,
+        },
+      },
+      blue: {
+        "2x2": {
+          quantity: 7,
+          price: 0.02,
+        },
+        "2x4": {
+          quantity: 2,
+          price: 0.04,
+        },
+      },
+      green: {
+        "2x4": {
+          quantity: 13,
+          price: 0.04,
+        },
+        "2x8": {
+          quantity: 41,
+          price: 0.08,
+        },
+      },
+    };
+    this.buy = function (color, size) {
+      this.bricks[color][size].quantity--;
+    };
+  });
