75c668873ab7c5f35577d390bda6aad51bce6f83
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-02 03:08:51 +0300

    feat@angular: lesson #34 solution

--- frontend-apps/10/34/test/karma.conf.js
+++ frontend-apps/10/34/test/karma.conf.js
@@ -22,10 +22,11 @@ module.exports = function (config) {
     // list of files / patterns to load in the browser
     files: [
       // bower:js
-      "bower_components/jquery/dist/jquery.js",
-      "bower_components/angular/angular.js",
-      "bower_components/bootstrap/dist/js/bootstrap.js",
-      "bower_components/angular-mocks/angular-mocks.js",
+      'bower_components/jquery/dist/jquery.js',
+      'bower_components/angular/angular.js',
+      'bower_components/bootstrap/dist/js/bootstrap.js',
+      'bower_components/angular-ui-router/release/angular-ui-router.js',
+      'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
       "test/mock/**/*.js",
