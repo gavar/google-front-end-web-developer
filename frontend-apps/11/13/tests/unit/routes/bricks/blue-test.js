36e26d9ffbe1323ef6fdeb2a29270a4f0c8e3774
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-04 02:06:53 +0300

    feat@ember: lesson #13 solution

---
+++ frontend-apps/11/13/tests/unit/routes/bricks/blue-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:bricks/blue', 'Unit | Route | bricks/blue', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
