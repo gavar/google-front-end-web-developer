3742138a2d11c5e5e96b80890da8138defebf0af
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-04 21:10:00 +0300

    feat@ember: lesson #25 solution

--- frontend-apps/11/25/app/routes/store/bricks-a-bunch.js
+++ frontend-apps/11/25/app/routes/store/bricks-a-bunch.js
@@ -1,20 +1,8 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  warehouse: Ember.inject.service("brick-warehouse"),
   model() {
-    return {
-      "1 x 1": {
-        quantity: 29,
-        price: 0.01,
-      },
-      "2 x 2": {
-        quantity: 48,
-        price: 0.03,
-      },
-      "2 x 6": {
-        quantity: 27,
-        price: 0.05,
-      },
-    };
+    return this.get("warehouse").getRedBricks();
   },
 });
--- frontend-apps/11/25/app/routes/store/lots-o-bricks.js
+++ frontend-apps/11/25/app/routes/store/lots-o-bricks.js
@@ -1,28 +1,8 @@
 import Ember from "ember";
 
 export default Ember.Route.extend({
+  warehouse: Ember.inject.service("brick-warehouse"),
   model() {
-    return {
-      blue: {
-        "2 x 2": {
-          quantity: 7,
-          price: 0.02,
-        },
-        "2 x 4": {
-          quantity: 2,
-          price: 0.04,
-        },
-      },
-      green: {
-        "2 x 4": {
-          quantity: 13,
-          price: 0.04,
-        },
-        "2 x 8": {
-          quantity: 41,
-          price: 0.08,
-        },
-      },
-    };
+    return this.get("warehouse").getBricks();
   },
 });
