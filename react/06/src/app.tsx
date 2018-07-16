6fe0a4eda5d40311c83ecedc6b672411d7a5c990
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-16 21:42:33 +0300

    fix: fixing linting errors

--- react/06/src/app.tsx
+++ react/06/src/app.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./app.css";
 
 class BooksApp extends React.Component {
+
   state = {
     /**
      * TODO: Instead of using this state variable to keep track of which page
@@ -13,13 +14,19 @@ class BooksApp extends React.Component {
     showSearchPage: false,
   };
 
+  /** @inheritDoc */
+  componentWillMount(): void {
+    this.openSearch = this.openSearch.bind(this);
+    this.closeSearch = this.closeSearch.bind(this);
+  }
+
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({showSearchPage: false})}>Close</a>
+              <a className="close-search" onClick={this.closeSearch}>Close</a>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -34,7 +41,7 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid"/>
             </div>
           </div>
         ) : (
@@ -55,7 +62,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 193,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -78,7 +85,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 188,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -108,7 +115,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 193,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=uu1mC6zWNTwC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73pGHfBNSsJG9Y8kRBpmLUft9O4BfItHioHolWNKOdLavw-SLcXADy3CPAfJ0_qMb18RmCa7Ds1cTdpM3dxAGJs8zfCfm8c6ggBIjzKT7XR5FIB53HHOhnsT7a0Cc-PpneWq9zX&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -131,7 +138,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 192,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=wrOQLV6xB-wC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72G3gA5A-Ka8XjOZGDFLAoUeMQBqZ9y-LCspZ2dzJTugcOcJ4C7FP0tDA8s1h9f480ISXuvYhA_ZpdvRArUL-mZyD4WW7CHyEqHYq9D3kGnrZCNiqxSRhry8TiFDCMWP61ujflB&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -162,7 +169,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 192,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=pD6arNyKyi8C&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE70Rw0CCwNZh0SsYpQTkMbvz23npqWeUoJvVbi_gXla2m2ie_ReMWPl0xoU8Quy9fk0Zhb3szmwe8cTe4k7DAbfQ45FEzr9T7Lk0XhVpEPBvwUAztOBJ6Y0QPZylo4VbB7K5iRSk&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -185,7 +192,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 174,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=1q_xAwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE712CA0cBYP8VKbEcIVEuFJRdX1k30rjLM29Y-dw_qU1urEZ2cQ42La3Jkw6KmzMmXIoLTr50SWTpw6VOGq1leINsnTdLc_S5a5sn9Hao2t5YT7Ax1RqtQDiPNHIyXP46Rrw3aL8&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -208,7 +215,7 @@ class BooksApp extends React.Component {
                               width: 128,
                               height: 192,
                               backgroundImage: "url(\"http://books.google.com/books/content?id=32haAAAAMAAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72yckZ5f5bDFVIf7BGPbjA0KYYtlQ__nWB-hI_YZmZ-fScYwFy4O_fWOcPwf-pgv3pPQNJP_sT5J_xOUciD8WaKmevh1rUR-1jk7g1aCD_KeJaOpjVu0cm_11BBIUXdxbFkVMdi&source=gbs_api\")",
-                            }}></div>
+                            }}/>
                             <div className="book-shelf-changer">
                               <select>
                                 <option value="move" disabled>Move to...</option>
@@ -230,12 +237,20 @@ class BooksApp extends React.Component {
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({showSearchPage: true})}>Add a book</a>
+              <a onClick={this.openSearch}>Add a book</a>
             </div>
           </div>
         )}
       </div>
     );
+  }
+
+  openSearch() {
+    this.setState({showSearchPage: true});
+  }
+
+  closeSearch() {
+    this.setState({showSearchPage: false});
   }
 }
 
