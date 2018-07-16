005ac64611e8cd4d42ef37f4d416cf7dfb0e54fd
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-16 21:26:48 +0300

    feat@my-reads: udacity source starter files

--- react/06/src/app.test.tsx
+++ react/06/src/app.test.tsx
@@ -1,9 +1,14 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./app";
 
+/**
+ * This course is not designed to teach Test Driven Development.
+ * Feel free to use this file to test your application, but it
+ * is not required.
+ */
+
 it("renders without crashing", () => {
-    const div = document.createElement("div");
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+  const div = document.createElement("div");
+  ReactDOM.render(<App/>, div);
 });
