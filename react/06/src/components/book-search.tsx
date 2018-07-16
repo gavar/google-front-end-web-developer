d9a93cb74651e70b1265e8ec03a2e218be4d3c3a
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-17 02:20:48 +0300

    feat: books search

--- react/06/src/components/book-search.tsx
+++ react/06/src/components/book-search.tsx
@@ -1,31 +1,91 @@
-import React, {Component} from "react";
+import React, {ChangeEvent, Component} from "react";
 import {Link} from "react-router-dom";
+import * as BooksAPI from "../books-api";
+import {Book} from "../types";
+import {BookView} from "./book-view";
 
-export interface BookSearchProps {
-
+export interface BookSearchState {
+    query: string;
+    books: Book[];
 }
 
-export class BookSearch extends Component<BookSearchProps> {
+export class BookSearch extends Component<{}, BookSearchState> {
+
+    state = {
+        query: "",
+        books: [],
+    } as BookSearchState;
+
+    /** @inheritDoc */
+    componentWillMount(): void {
+        this.onQueryChange = this.onQueryChange.bind(this);
+        this.changeBookShelf = this.changeBookShelf.bind(this);
+    }
+
     render() {
+        const {books} = this.state;
         return <div className="search-books">
             <div className="search-books-bar">
                 <Link to="/" className="close-search"/>
                 <div className="search-books-input-wrapper">
-                    {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                    <input type="text" placeholder="Search by title or author"/>
-
+                    <input type="text" placeholder="Search by title or author" onChange={this.onQueryChange}/>
                 </div>
             </div>
             <div className="search-books-results">
-                <ol className="books-grid"/>
+                <ol className="books-grid">
+                    {books && books.map(book => (
+                        <li key={book.id}>
+                            <BookView book={book} changeBookShelf={this.changeBookShelf}/>
+                        </li>
+                    ))}
+                </ol>
             </div>
         </div>;
+    }
+
+    async onQueryChange(e: ChangeEvent<HTMLInputElement>) {
+        // save query
+        const query = e.target.value.trim();
+        this.setState({query});
+
+        // clear books if empty search
+        if (!query.length)
+            return this.setState({books: []});
+
+        try {
+            // query for search results
+            const books = await BooksAPI.search(query);
+
+            // clear books if error
+            if (books.error) {
+                console.log(books);
+                return this.setState({books: []});
+            }
+
+            // modify state if still have same query
+            return this.setState(prev => {
+                if (prev.query === query)
+                    return {...prev, books};
+
+                return prev;
+            });
+        }
+        catch (e) {
+            // clear books if error
+            this.setState({books: []});
+            console.error(e);
+        }
+    }
+
+    async changeBookShelf(book: Book, shelf: string) {
+        // check if really changed
+        if (shelf === book.shelf)
+            return;
+
+        // update book shelf locally
+        book.shelf = shelf;
+
+        // update book shelf on a server
+        await BooksAPI.update(book, shelf);
     }
 }
