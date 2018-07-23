eecb6c3eb9a205e67e08f3e6cf10ea7ad0b402db
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-23 20:48:11 +0300

    fix: lowercase css

--- react/10/src/app.tsx
+++ react/10/src/app.tsx
@@ -6,12 +6,12 @@ import logo from "./logo.svg";
 export class App extends React.Component {
     public render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+            <div className="app">
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo"/>
+                    <h1 className="app-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">
+                <p className="app-intro">
                     To get started, edit <code>src/App.tsx</code> and save to reload.
                 </p>
             </div>
