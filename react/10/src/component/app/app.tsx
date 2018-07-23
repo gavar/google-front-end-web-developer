8360bab9267fb31835ccc4d1316c89c0e989b218
Max Stankevich <max.stankevich.me@gmail.com>
2018-07-23 21:02:05 +0300

    refactor: move app component to own directory

--- react/10/src/component/app/app.tsx
+++ react/10/src/component/app/app.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import "./app.css";
 
-import logo from "./logo.svg";
+import logo from "../../logo.svg";
+import "./app.css";
 
 export class App extends React.Component {
     public render() {
