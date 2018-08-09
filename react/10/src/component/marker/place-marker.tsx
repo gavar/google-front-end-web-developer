dd5a506e061984b24ddb10a6cfce31d9cb1f730f
Max Stankevich <max.stankevich.me@gmail.com>
2018-08-09 16:13:09 +0300

    feat: use MarkerWithLabel to display styleable SVG marker

--- react/10/src/component/marker/place-marker.tsx
+++ react/10/src/component/marker/place-marker.tsx
@@ -1,68 +1,79 @@
+import {Point} from "$google/maps";
+import {classNames} from "$util";
 import {autobind} from "core-decorators";
 import React, {PureComponent} from "react";
-import {Marker} from "react-google-maps";
+import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
 import {Place} from "../../service";
-import {PlaceMarkerInfo} from "./place-marker-info";
+import "./place-marker.scss";
+import PlaceMarkerPin from "./place-marker.svg";
 
 export interface PlaceMarkerProps {
     place: Place;
+    hover?: boolean;
     active?: boolean;
+    onMouseOver?(marker: Place);
+    onMouseOut?(marker: Place);
     onSelect?(marker: Place);
-    onDestroy?(marker: Place);
 }
 
-export interface PlaceMarkerState {
-    info: boolean;
+interface PlaceMarkerState {
+
 }
+
+let AnchorPoint: Point;
+const markerPin = <PlaceMarkerPin className="place-marker-pin"/>;
 
 export class PlaceMarker extends PureComponent<PlaceMarkerProps, PlaceMarkerState> {
 
-    static readonly defaultProps: Partial<PlaceMarkerProps> = {
-        active: true,
-    };
-
-    /** @inheritDoc */
-    state = {
-        info: false,
-    };
-
-    /** @inheritDoc */
-    componentWillUnmount(): void {
-        const {place, onDestroy} = this.props;
-        if (onDestroy) onDestroy(place);
-    }
-
-    componentDidUpdate(prev: Readonly<PlaceMarkerProps>): void {
-        const {active} = this.props;
-        if (active !== prev.active)
-            if (!active) this.setShowInfo(false);
+    constructor(props, context) {
+        AnchorPoint = AnchorPoint || new google.maps.Point(16, 48);
+        super(props, context);
+        this.state = {hover: false};
     }
 
     /** @inheritDoc */
     render() {
-        const {info} = this.state;
         const {place, active} = this.props;
         const {location, key} = place;
-        const showInfo = info && active;
+        const hover = this.props.hover;
 
-        return <Marker position={location}
-                       place={{placeId: key, location: location}}
-                       onClick={this.toggleInfo}>
-            {showInfo && <PlaceMarkerInfo
-                placeKey={key}
-                onCloseClick={this.toggleInfo}/>}
-        </Marker>;
+        const z = hover ? 1001 : active ? 1000 : void 0;
+        const className = classNames(
+            "place-marker",
+            active ? "active" : hover && "hover",
+        );
+
+        return <MarkerWithLabel key={key}
+                                zIndex={z}
+                                place={{placeId: key, location: location}}
+                                position={location}
+                                icon={" "} // hide default marker icon
+                                labelClass={className}
+                                labelAnchor={AnchorPoint}
+                                onMouseOver={this.onMouseOver}
+                                onMouseOut={this.onMouseOut}
+                                onClick={this.onClick}>
+            <div className="place-marker-container">
+                {markerPin}
+            </div>
+        </MarkerWithLabel>;
     }
 
     @autobind
-    protected toggleInfo() {
-        const {info} = this.state;
-        this.setShowInfo(!info);
+    protected onClick() {
+        const {place, onSelect} = this.props;
+        if (onSelect) onSelect(place);
     }
 
-    private setShowInfo(value: boolean) {
-        const {place, onSelect} = this.props;
-        if (value) onSelect && onSelect(place);
-        this.setState({info: value});
+    @autobind
+    protected onMouseOver() {
+        const {place, onMouseOver} = this.props;
+        if (onMouseOver) onMouseOver(place);
+    }
+
+    @autobind
+    protected onMouseOut() {
+        const {place, onMouseOut} = this.props;
+        if (onMouseOut) onMouseOut(place);
     }
 }
