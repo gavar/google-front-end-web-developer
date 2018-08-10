67235c199beb66d564c0f95ed44bcfded6fc20d5
Max Stankevich <max.stankevich.me@gmail.com>
2018-08-11 01:18:47 +0300

    fix: service worker typings

--- react/10/src/sw/types.d.ts
+++ react/10/src/sw/types.d.ts
@@ -1,3 +1,6 @@
+/// <reference path="../../node_modules/typescript/lib/lib.es2015.d.ts" />
+/// <reference path="../../node_modules/typescript/lib/lib.webworker.d.ts" />
+
 declare interface WorkerLocation {
     username: string,
     password: string,
