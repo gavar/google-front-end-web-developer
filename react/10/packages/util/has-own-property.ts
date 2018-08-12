edbf25d28bfa84901b9db7e5fa879ff730359be6
Max Stankevich <max.stankevich.me@gmail.com>
2018-08-12 17:08:18 +0300

    remove: removing unused utils

--- react/10/packages/util/has-own-property.ts
+++
@@ -1,6 +0,0 @@
-export function hasOwnProperty(object: object, propertyName?: string): boolean {
-    for (const key in object)
-        if (object.hasOwnProperty(key))
-            if (!propertyName || key === propertyName)
-                return true;
-}
--- react/10/packages/util/index.ts
+++ react/10/packages/util/index.ts
@@ -1,4 +1,2 @@
-export * from "./uniq";
 export * from "./identity";
 export * from "./class-names";
-export * from "./has-own-property";
